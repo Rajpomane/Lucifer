564bab98eff391ccbe89f7c86460aca5c4945061
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-03-31 03:25:50 +0530

    Update __main__.py

--- Lucifer/__main__.py
+++ Lucifer/__main__.py
@@ -58,7 +58,7 @@ for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+        load_module(shortname.replace(".py"))
 
 if LOAD_MYBOT == "True":
     path = "Lucifer/plugins/mybot/pmbot/*.py"
