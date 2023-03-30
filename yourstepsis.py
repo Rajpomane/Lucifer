e311387b1d9c8f1e05f4777c9bad62722778f141
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-03-31 02:35:00 +0530

    Update yourstepsis.py

--- yourstepsis.py
+++ yourstepsis.py
@@ -1,6 +1,5 @@
 from os import system
 system("git clone https://gitHub.com/kaal0408/Lucifer && cd Lucifer && python3 -m Lucifer")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @Lucifer_support_group")
 # Script written for stop heroku suspension
 # use with credit else gay
